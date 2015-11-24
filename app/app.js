ce73431eff1d854706a43dad677acf1e1e57794d
amhayslip <amhayslip@gmail.com>
2015-11-24 17:41:48 +0200

    hello world

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
 var angular = require('angular');
+
+var app = angular.module('beerReview', []);
+
+app.controller('MainController', [
+  '$scope',
+function($scope){
+  $scope.hello = "Hello World!";
+}]);
